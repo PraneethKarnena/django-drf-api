d4202a018177cb4bb1cdad5e757dfa858f860051
Praneeth Karnena <praneeth.codes@gmail.com>
2019-08-02 00:15:30 +0530

    added URL for sending a request

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('isd/', views.insert_sample_data),
     path('get-patients/', views.initial_patient_details),
     path('get-doctors/', views.doctors_details),
+    path('send-request/<int:patient_id>/<int:doctor_id>/', views.send_request),
 ]
 
