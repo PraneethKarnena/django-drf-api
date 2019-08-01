dbbfdbd525f86ca8d106ebd04958e1857ce6de29
Praneeth Karnena <praneeth.codes@gmail.com>
2019-08-01 23:15:00 +0530

    added intital patient data API

--- api/views.py
+++ api/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from . import models
 from faker import Faker
+from rest_framework.response import Response
+from rest_framework import status
+from . import models
+from . import serializers
 
 
 def insert_sample_data(request):
@@ -25,3 +28,9 @@ def insert_sample_data(request):
         )
 
     return HttpResponse('Ok!')
+
+
+def initial_patient_details(request):
+    patients = models.PatientModel.objects.all()
+    patients_serializer = serializers.InitialPatientSerializer(patients, many=True)
+    return Response(data={'data': patients_serializer.data}, status=status.HTTP_200_OK)
