f31e0b2c906a5dcbf1e3f67a841163dd858d1ee0
Praneeth Karnena <praneeth.codes@gmail.com>
2019-08-01 23:50:18 +0530

    added friends key

--- api/models.py
+++ api/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 
+class DoctorModel(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f'{self.name} - {self.email}'
+
+
 class PatientModel(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField()
@@ -10,14 +18,7 @@ class PatientModel(models.Model):
     mobile = models.CharField(max_length=12)
     dob = models.DateField()
     blood_group = models.CharField(max_length=3)
-
-    def __str__(self):
-        return f'{self.name} - {self.email}'
-
-
-class DoctorModel(models.Model):
-    name = models.CharField(max_length=255)
-    email = models.EmailField()
+    friends = models.ManyToManyField(DoctorModel, blank=True)
 
     def __str__(self):
         return f'{self.name} - {self.email}'
