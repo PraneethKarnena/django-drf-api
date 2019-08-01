cca560411b1662ec3a3bf315d4873c39f5d7c0dc
Praneeth Karnena <praneeth.codes@gmail.com>
2019-08-02 00:00:04 +0530

    added friend model

--- api/models.py
+++ api/models.py
@@ -18,7 +18,20 @@ class PatientModel(models.Model):
     mobile = models.CharField(max_length=12)
     dob = models.DateField()
     blood_group = models.CharField(max_length=3)
-    friends = models.ManyToManyField(DoctorModel, blank=True)
 
     def __str__(self):
         return f'{self.name} - {self.email}'
+
+
+class FriendsModel(models.Model):
+    doctor = models.ForeignKey(DoctorModel, on_delete=models.CASCADE)
+    patient = models.ForeignKey(PatientModel, on_delete=models.CASCADE)
+    STATUS_CHOICES = (
+        ('PEN', 'Pending'),
+        ('ACC', 'Accepted'),
+        ('REJ', 'Rejected'),
+    )
+    status = models.CharField(max_length=3, choices=STATUS_CHOICES, default='PEN')
+
+    def __str__(self):
+        return f'{str(self.doctor.name)} - {str(self.patient.name)} - {self.status}'
