68e058cbe6e95470a9cb7ca54cc6fabb0d5a70df
Praneeth Karnena <praneeth.codes@gmail.com>
2019-08-01 23:29:40 +0530

    added serializer for doctors

--- api/serializers.py
+++ api/serializers.py
@@ -6,3 +6,10 @@ class InitialPatientSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.PatientModel
         fields = ['name', 'id']
+
+
+class DoctorsSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.DoctorModel
+        fields = '__all__'
