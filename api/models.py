aad2566e3e0ae100659b9403e6177ac43e964efe
Praneeth Karnena <praneeth.codes@gmail.com>
2019-08-04 12:35:20 +0530

    Heroku error fixes

--- api/models.py
+++ api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class DoctorModel(models.Model):
     name = models.CharField(max_length=255)
-    email = models.EmailField()
+    email = models.EmailField(max_length=255)
 
     def __str__(self):
         return f'{self.name} - {self.email}'
@@ -11,13 +11,13 @@ class DoctorModel(models.Model):
 
 class PatientModel(models.Model):
     name = models.CharField(max_length=255)
-    email = models.EmailField()
+    email = models.EmailField(max_length=255)
     address = models.TextField()
     company = models.TextField()
     job = models.TextField()
-    mobile = models.CharField(max_length=12)
+    mobile = models.CharField(max_length=255)
     dob = models.DateField()
-    blood_group = models.CharField(max_length=6)
+    blood_group = models.CharField(max_length=255)
 
     def __str__(self):
         return f'{self.name} - {self.email}'
